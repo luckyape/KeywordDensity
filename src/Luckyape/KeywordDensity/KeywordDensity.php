e8c391990b591a8eb051ff37867ab658339ee8dd
graham charles <graham@luckyape.com>
2014-08-21 12:37:01 -0700

    update namespace

--- src/Luckyape/KeywordDensity/KeywordDensity.php
+++ src/Luckyape/KeywordDensity/KeywordDensity.php
@@ -1,9 +1,9 @@
 <?php
 
 
-namespace KeywordDensity;
+namespace Luckyape\KeywordDensity;
 
-use Luckyape\Html2Text
+use BloggerFly\Html2Text
 /*
  ======================================================================
  KeywordDensityChecker v1.1
